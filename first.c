6d9fe441001e503fe3f4e8fee365f9a3912a2622
omais lateef <babaomais4@gmail.com>
2026-04-17 12:52:55 +0530

    Add files via upload

---
+++ first.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+// int main(){
+// int n;
+// printf("enter number");
+// scanf("%d",&n);
+// for(int i=0 ; i<=n ; i++){
+//     if(n%i == 0){
+//         printf("%d\n",i);
+//     }
+// }
+// return 0;
+// } 
+int main(){
+    int a=10;
+    int b;
+     printf("enter number");
+     scanf("%d",&b);
+     int sum = a+b;
+     printf("%d",sum);
+
+}
